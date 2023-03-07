15586a24bda61d578d42f30095e090667358b8c9
Manuel Pastor Ringuelet <pastorringuelet@hotmail.com>
2023-03-07 17:06:20 +0100

    header

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-import './style.css'
+import "./style.css";
+
+function myFunction() {
+  let x = document.getElementById("bottonnav")!;
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
