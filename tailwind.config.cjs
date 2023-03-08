5c93ed31e5710099e7960c9a0627389d8a1fe51d
Manuel Pastor Ringuelet <pastorringuelet@hotmail.com>
2023-03-08 13:15:18 +0100

    section 1 changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "950px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      backgroundColor: {
+        "neutral-850": "#1D1D1D",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "3.25rem",
+      },
+      borderWidth: {
+        "1": "1px",
+      },
+    },
   },
   plugins: [],
-}
+};
